80508033b3b6ab339530121ae6a4ad01a885c289
ManikandanS2002 <manikandans20052002@gmail.com>
2024-06-14 13:57:36 +0530

    netlify file

--- mern-client/vite.config.js
+++ mern-client/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: '/your-repo-name/', // Adjust if your app is in a subdirectory
+  plugins: [reactRefresh()],
+  build: {
+    outDir: 'mern-client/dist', // Output directory inside mern-client
+  }
+});
+
